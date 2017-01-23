75c62dde3bec4cfdac5af8878ed8e24523a26e7e
Lance Hartung <hartung@cs.wisc.edu>
2017-01-23 12:53:39 -0600

    Fixed ping command.

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ iw dev ${INTERFACE:-mon0} set channel ${CHANNEL:-1}
 ifconfig $dev up
 
 tcpdump -i ${INTERFACE:-mon0} -w /data/monitor_$timestamp.cap &
-ping -i 0.2 ${PING_HOST:-8.8.8.8} >$/data/pings_$timestamp.txt &
+ping -i 0.2 ${PING_HOST:-8.8.8.8} >/data/pings_$timestamp.txt &
 
 while true; do
     sync
