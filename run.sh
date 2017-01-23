a7c92bfa550c4abd9f17dc2af22cc686b5d952e2
Lance Hartung <hartung@cs.wisc.edu>
2017-01-23 12:50:47 -0600

    Hopefully fixed.

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 timestamp=$(date +%Y%m%d_%H%M%S)
+dev=${INTERFACE:-mon0}
 
+sleep 15s
+
+ifconfig $dev down
 iw dev ${INTERFACE:-mon0} set channel ${CHANNEL:-1}
+ifconfig $dev up
+
 tcpdump -i ${INTERFACE:-mon0} -w /data/monitor_$timestamp.cap &
 ping -i 0.2 ${PING_HOST:-8.8.8.8} >$/data/pings_$timestamp.txt &
 
